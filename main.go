3b27516cb599eba64d5438efbb71397cb44f3fc4
Jeferson <jefersonnpn@gmail.com>
2023-09-14 19:25:11 -0300

    :sparkles:  parses data from file

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
+	"strings"
+	"turminha/turma"
+	"turminha/types"
 )
 
 func main() {
-	parseData()
+	//data := parseData()
 }
 
-func parseData() {
+func parseData() *turma.Turma {
 	turmaFile, err := os.Open("data/turma.csv")
 	checkErr(err)
 
@@ -20,9 +22,18 @@ func parseData() {
 	// Skip first since it is a header
 	turmaScanner.Scan()
 
+	t := turma.New()
+
 	for turmaScanner.Scan() {
-		fmt.Println(turmaScanner.Text())
+		s := strings.Split(turmaScanner.Text(), ",")
+		t.AddChild(types.Child{
+			FullName:  s[0],
+			BirthDate: s[1],
+			Genre:     s[3],
+		})
 	}
+
+	return t
 }
 
 func checkErr(err error) {
