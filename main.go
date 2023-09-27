d2dde9e6b78800176474860cb845c522b439849f
Jeferson <jefersonnpn@gmail.com>
2023-09-26 21:14:33 -0300

    :truck:  reorganizing..

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 	"text/template"
-	"time"
 
 	"turminha/turma"
+	"turminha/util"
 
 	"golang.org/x/exp/slices"
 )
@@ -20,7 +17,7 @@ var (
 )
 
 func main() {
-	data := parseData()
+	data := util.ParseData()
 
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
@@ -52,57 +49,4 @@ func main() {
 
 	log.Println("server running at", listenAddr)
 	panic(http.ListenAndServe(listenAddr, nil))
-}
-
-func parseData() *turma.Turma {
-	turmaFile, err := os.Open("data/turma.csv")
-	checkErr(err)
-
-	turmaScanner := bufio.NewScanner(turmaFile)
-	turmaScanner.Split(bufio.ScanLines)
-
-	// Skip first since it is the header
-	turmaScanner.Scan()
-
-	t := turma.New()
-
-	for turmaScanner.Scan() {
-		s := strings.Split(strings.Trim(turmaScanner.Text(), "\"'"), ",")
-
-		fullname := strings.Trim(s[0], "\"'")
-		name, _ := turma.GetFirstAndLastName(s[0])
-
-		b, err := time.Parse("02/01/2006", strings.Trim(s[1], "\"'"))
-		if err != nil {
-			panic(err)
-		}
-		childAgeInMinutes := time.Since(b).Minutes()
-		childAge := childAgeInMinutes / (60 * 24 * 365)
-		birthDate := b.Format("Jan 02, 2006")
-
-		var genre string
-		s[3] = strings.Trim(s[3], "\"'")
-		if strings.Compare(s[3], "M") == 0 {
-			genre = "Boy"
-		} else if strings.Compare(s[3], "F") == 0 {
-			genre = "Girl"
-		}
-
-		t.AddChild(turma.Child{
-			FullName:     fullname,
-			Name:         name,
-			BirthDate:    birthDate,
-			Age:          int(childAge),
-			AgeInMinutes: childAgeInMinutes,
-			Genre:        genre,
-		})
-	}
-
-	return t
-}
-
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
---
+++ util/dataparse.go
@@ -0,0 +1,62 @@
+package util
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"time"
+	"turminha/turma"
+)
+
+func ParseData() *turma.Turma {
+	turmaFile, err := os.Open("data/turma.csv")
+	checkErr(err)
+
+	turmaScanner := bufio.NewScanner(turmaFile)
+	turmaScanner.Split(bufio.ScanLines)
+
+	// Skip first since it is the header
+	turmaScanner.Scan()
+
+	t := turma.New()
+
+	for turmaScanner.Scan() {
+		s := strings.Split(strings.Trim(turmaScanner.Text(), "\"'"), ",")
+
+		fullname := strings.Trim(s[0], "\"'")
+		name, _ := turma.GetFirstAndLastName(s[0])
+
+		b, err := time.Parse("02/01/2006", strings.Trim(s[1], "\"'"))
+		if err != nil {
+			panic(err)
+		}
+		childAgeInMinutes := time.Since(b).Minutes()
+		childAge := childAgeInMinutes / (60 * 24 * 365)
+		birthDate := b.Format("Jan 02, 2006")
+
+		var genre string
+		s[3] = strings.Trim(s[3], "\"'")
+		if strings.Compare(s[3], "M") == 0 {
+			genre = "Boy"
+		} else if strings.Compare(s[3], "F") == 0 {
+			genre = "Girl"
+		}
+
+		t.AddChild(turma.Child{
+			FullName:     fullname,
+			Name:         name,
+			BirthDate:    birthDate,
+			Age:          int(childAge),
+			AgeInMinutes: childAgeInMinutes,
+			Genre:        genre,
+		})
+	}
+
+	return t
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
