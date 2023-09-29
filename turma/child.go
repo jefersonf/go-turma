2023aae17cb0cd7a819b044f17e466b123df36a0
Jeferson <jefersonnpn@gmail.com>
2023-09-29 17:17:53 -0300

    :sparkles:  AgeInMinutes becomes a pointer-receiver

--- turma/child.go
+++ turma/child.go
@@ -1,10 +1,25 @@
 package turma
 
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
 type Child struct {
-	FullName     string
-	Name         string
-	Age          int
-	AgeInMinutes float64
-	BirthDate    string
-	Genre        string
+	FullName  string
+	Name      string
+	Age       int
+	BirthDate string
+	Genre     string
+}
+
+func (c *Child) AgeInMinutes() float64 {
+	b, err := time.Parse("Jan 02, 2006", c.BirthDate)
+	if err != nil {
+		fmt.Printf("unable to parse bith date: %v\n", err)
+	}
+	toMinutes := time.Since(b).Minutes()
+	ageInMinutes, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", toMinutes), 64)
+	return ageInMinutes
 }
