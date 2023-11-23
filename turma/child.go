ef056acb8cfe639d44e8e12ee3e139c09c99c4b0
Jeferson Ferreira <jefersonnpn@gmail.com>
2023-11-22 23:47:46 -0300

    Update child.go

--- turma/child.go
+++ turma/child.go
@@ -17,7 +17,7 @@ type Child struct {
 func (c *Child) AgeInMinutes() float64 {
 	b, err := time.Parse("Jan 02, 2006", c.BirthDate)
 	if err != nil {
-		fmt.Printf("unable to parse bith date: %v\n", err)
+		fmt.Printf("unable to parse birth date: %v\n", err)
 	}
 	toMinutes := time.Since(b).Minutes()
 	ageInMinutes, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", toMinutes), 64)
